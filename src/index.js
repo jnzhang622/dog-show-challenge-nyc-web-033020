9dfa5b1610133339401c782764163965928a08e2
jnzhang622 <jnzhang622@gmail.com>
2020-05-20 10:25:14 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+const dogForm = document.querySelector('dog-form')
+
+    function getDogs(){ 
+        return fetch('http://localhost:3000/dogs')
+        .then (resp => resp.json())
+
+    }
+
 
 })
